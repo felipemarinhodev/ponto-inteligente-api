c756da1429f89b3f62919738e385ae3c4387a53a
Felipe Marinho <felipmarinho@gmail.com>
2018-08-16 05:11:08 -0300

    Refatorando codigo

--- src/main/java/br/com/felipefm/pontointeligente/api/controller/LancamentoController.java
+++ src/main/java/br/com/felipefm/pontointeligente/api/controller/LancamentoController.java
@@ -15,7 +15,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.CrossOrigin;
