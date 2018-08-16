f947a7c4d8284e8ff381490f1a21fcde172e82ce
Felipe Marinho <felipmarinho@gmail.com>
2018-08-16 20:41:41 -0300

    Adicionando o EhCache

--- src/main/java/br/com/felipefm/pontointeligente/api/PontoInteligenteApplication.java
+++ src/main/java/br/com/felipefm/pontointeligente/api/PontoInteligenteApplication.java
@@ -2,7 +2,9 @@ package br.com.felipefm.pontointeligente.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
+@EnableCaching
 @SpringBootApplication
 public class PontoInteligenteApplication {
 
